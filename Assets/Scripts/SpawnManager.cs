1ab1eb2e45a55af959f0bc7740f706304ebfe42a
Nur Yl <52735279+nuryll@users.noreply.github.com>
2023-09-25 13:53:36 +0200

    Add Repeat Background

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -12,6 +12,7 @@ public class SpawnManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+
         InvokeRepeating("SpawnObstacle", startDelay, repeatRate);
     }
 
